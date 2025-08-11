9196c8b193b8bed846f8a12e531b6e3c38610276
mniip <mniip@users.noreply.github.com>
2025-08-11 22:54:31 +0200

    Logging (#6)

--- bot/database/pool.py
+++ bot/database/pool.py
@@ -1,5 +1,17 @@
 import asyncpg
 
+import bot.database.logging
+
+
+async def _init_connection(connection: asyncpg.Connection) -> None:
+    connection.add_query_logger(bot.database.logging.log_query)
+
 
 async def create_database_pool(*, database_connection_string: str) -> asyncpg.Pool:
-    return await asyncpg.create_pool(dsn=database_connection_string, min_size=10, max_size=20)
+    pool = await asyncpg.create_pool(
+        dsn=database_connection_string,
+        init=_init_connection,
+        min_size=10,
+        max_size=20,
+    )
+    return pool
