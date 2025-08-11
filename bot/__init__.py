cf76f51f0da35ddf75bfdb9504971574636de652
mniip <mniip@mniip.com>
2025-08-11 22:00:28 +0200

    Add JSON logging

--- bot/__init__.py
+++ bot/__init__.py
@@ -4,13 +4,16 @@ accepting parameters that describe how the bot is deployed and where to find ser
 """
 
 import asyncio
+import logging
 import os
+
+from pythonjsonlogger.json import JsonFormatter
 
 from bot.config.bot import ConfigStore
 from bot.database.pool import create_database_pool
 
 
-def get_database_connection_string():
+def get_database_connection_string() -> str:
     try:
         return os.environ["DATABASE"]
     except KeyError:
@@ -20,11 +23,35 @@ def get_database_connection_string():
         )
 
 
+def setup_logging() -> None:
+    handler = logging.StreamHandler()
+    handler.setFormatter(
+        JsonFormatter(
+            # The text portion of this format string is ignored, but the list of placeholders used determines which keys
+            # are put into the logged JSON object.
+            # Note that exc_info and stack_info are already included (when available).
+            # {name} is the Logger name (usually name of module where it is created)
+            "{asctime}{levelname}{name}{taskName}{module}{funcName}{lineno}{message}",
+            style="{",
+        )
+    )
+    logging.basicConfig(
+        force=True,
+        level=logging.NOTSET,
+        handlers=(handler,),
+    )
+
+
 async def _async_main():
+    task = asyncio.current_task()
+    assert task
+    task.set_name("Main Task")
+
     pool = await create_database_pool(database_connection_string=get_database_connection_string())
     store = ConfigStore(pool)
-    print(await store.get_bot_config())
+    logging.info(await store.get_bot_config())
 
 
 def main():
+    setup_logging()
     asyncio.run(_async_main())
