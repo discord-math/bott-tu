4384ac558801962ab248203b4fef6a08b99caada
mniip <mniip@mniip.com>
2026-01-09 06:13:07 +0100

    Document bot.database.logging

--- bot/database/logging.py
+++ bot/database/logging.py
@@ -1,3 +1,7 @@
+"""
+A callback that logs raw SQL statements is defined here, so that all raw SQL logs are attributed to this module.
+"""
+
 import logging
 
 import asyncpg
@@ -7,6 +11,7 @@ logger = logging.getLogger(__name__)
 
 
 def log_query(query: asyncpg.connection.LoggedQuery) -> None:
+    """A logging callback automatically installed by :func:`bot.database.pool.create_database_pool`."""
     kwargs: dict[str, object] = {}
     if query.args:
         kwargs["args"] = query.args
